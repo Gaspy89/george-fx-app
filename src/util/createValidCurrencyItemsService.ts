25ed6845d51d0eb886b4a1b4267ea54b5c589954
Marton Gaspardy <gaspy89@gmail.com>
2021-02-15 18:35:00 +0100

    renamed util function

--- src/util/createValidCurrencyItemsService.ts
+++ src/util/createValidCurrencyItemsService.ts
@@ -3,7 +3,7 @@ import {ValidCurrencyItem} from "../model/ValidCurrencyItem";
 
 export const createValidCurrencyItemsService = (items: CurrencyItem[]): ValidCurrencyItem[] => {
     const validItems: ValidCurrencyItem[] = [];
-    items.filter(itemContainsCurrencyNameCountryNameExchangeRate)
+    items.filter(itemContainsCurrencyCodeCurrencyNameExchangeRate)
         .map((currencyItem) => {
             currencyItem.flagUrl = preparesFlagUrl((currencyItem.currency as string).toLowerCase());
             validItems.push(currencyItem as ValidCurrencyItem);
@@ -15,7 +15,7 @@ export const preparesFlagUrl = (url: string): string => {
     return "/flags/" + url?.slice(0, -1) + ".png";
 }
 
-export const itemContainsCurrencyNameCountryNameExchangeRate = (currencyItem: CurrencyItem): boolean => {
+export const itemContainsCurrencyCodeCurrencyNameExchangeRate = (currencyItem: CurrencyItem): boolean => {
     return Boolean(currencyItem.currency) && Boolean(currencyItem.nameI18N) && Boolean(currencyItem.exchangeRate);
 }
 
